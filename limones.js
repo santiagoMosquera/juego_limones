d3d914528007be7fedf04541504ddcab4310b36d
santiagoMosquera <zantycb89@gmail.com>
2026-04-13 09:11:33 -0500

    copio de un alumno

--- limones.js
+++ limones.js
@@ -1,84 +1,123 @@
-let canvas=document.getElementById("areaJuego");
+let canvas=document.getElementById("areajuego");
 let ctx=canvas.getContext("2d");
 
 const ALTURA_SUELO=20;
 const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
-const ANCHO_LIMON=20;
 const ALTURA_LIMON=20;
-
+const ANCHO_LIMON=20;
 
 let personajeX=canvas.width/2;
-let personajeY=canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE);
+let personajeY=canvas.height - (ALTURA_SUELO + ALTURA_PERSONAJE);
 let limonX=canvas.width/2;
-let limonY=0;
+let limonY=5;
+let puntaje=0;
+let vidas=3;
+let velocidadcaida=200;
+let intervalo;
 
 function iniciar(){
+    intervalo = setInterval(bajarLimon, velocidadcaida);
     dibujarSuelo();
     dibujarPersonaje();
     aparecerLimon();
 }
+
 function dibujarSuelo(){
-    ctx.fillStyle="blue";
-    ctx.fillRect(0,canvas.height-ALTURA_SUELO,canvas.width,ALTURA_SUELO);
+    ctx.fillStyle="#8B4513";
+    ctx.fillRect(0, canvas.height - ALTURA_SUELO, canvas.width, ALTURA_SUELO);
 }
 
 function dibujarPersonaje(){
-    ctx.fillStyle="yellow";
-    ctx.fillRect(personajeX,personajeY,ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+    ctx.fillStyle="#ffe600";
+    ctx.fillRect(personajeX, personajeY, ANCHO_PERSONAJE, ALTURA_PERSONAJE);
 }
 
 function moverIzquierda(){
     personajeX=personajeX-10;
     actualizarPantalla();
-   
 }
-//moverDerecha
+
 function moverDerecha(){
     personajeX=personajeX+10;
     actualizarPantalla();
-  
 }
 
 function actualizarPantalla(){
-    limpiarCanva();
+    limpiarCanvas();
     dibujarSuelo();
     dibujarPersonaje();
     dibujarLimon();
 }
 
-function limpiarCanva(){
+function limpiarCanvas(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
 }
 
 function dibujarLimon(){
-    ctx.fillStyle="green";
-    ctx.fillRect(limonX,limonY,ANCHO_LIMON,ALTURA_LIMON);
+    ctx.fillStyle="#00ff00";
+    ctx.fillRect(limonX, limonY, ANCHO_LIMON, ALTURA_LIMON);
 }
 
 function bajarLimon(){
-    limonY = limonY + 10;
-    actualizarPantalla();  
-    detectarColision();
+    limonY=limonY+10;
+    actualizarPantalla();
+    detectarAtrapado();
+    detectarPiso();
 }
 
-function detectarColision(){
-    if(limonX+ANCHO_LIMON>personajeX && 
-        limonX < personajeX+ANCHO_PERSONAJE &&
-        limonY+ALTURA_LIMON>personajeY && 
-        limonY < personajeY+ALTURA_PERSONAJE){
-        //alert("ATRAPADO!!");
-        aparecerLimon();
+function detectarAtrapado(){
+    if(limonX+ANCHO_LIMON>personajeX &&
+       limonX < personajeX+ANCHO_PERSONAJE &&
+       limonY+ALTURA_LIMON>personajeY &&
+       limonY < personajeY+ALTURA_PERSONAJE) {
+       aparecerLimon();
+       puntaje=puntaje+1;
+       mostrarEnSpan("txtPuntaje", puntaje);
+    }
+
+    if(puntaje === 3){
+        velocidadcaida = 150;
+        clearInterval(intervalo);                          
+        intervalo = setInterval(bajarLimon, velocidadcaida); 
+    } else if(puntaje === 6){
+        velocidadcaida = 100;
+        clearInterval(intervalo);                         
+        intervalo = setInterval(bajarLimon, velocidadcaida); 
+    } else if(puntaje === 10){
+        clearInterval(intervalo);
+        alert("TIENES LOS LIMONES; AHORA TE FALTA SAL Y TEQUILA");
     }
 }
 
-function probarAleatorio(){
-    let aleatorio=generarAleatorio(10,80);
-    console.log(aleatorio);
-}
+function detectarPiso(){
+    if(limonY+ALTURA_LIMON>canvas.height-ALTURA_SUELO){
+        aparecerLimon();
+        vidas=vidas-1;
+        mostrarEnSpan("txtVidas", vidas);    
+    }
+
+    if(vidas === 0){
+        clearInterval(intervalo);
+        alert("GAME OVER");
+    }
+}    
 
 function aparecerLimon(){
-    limonX=generarAleatorio(0,canvas.width-ANCHO_LIMON);
+    limonX=generarAleatorio(0, canvas.width-ANCHO_LIMON);
     limonY=0;
     actualizarPantalla();
+}
+
+function reiniciar(){
+    clearInterval(intervalo);
+    puntaje = 0;
+    vidas = 3;
+    velocidadcaida = 200;
+    personajeX = canvas.width / 2;
+    limonX = canvas.width / 2;
+    limonY = 5;
+    mostrarEnSpan("txtPuntaje", puntaje);
+    mostrarEnSpan("txtVidas", vidas);
+    iniciar();
 }
--- utils.js
+++ utils.js
@@ -1,7 +1,12 @@
-function generarAleatorio(min,max){
+function generarAleatorio(min, max){
     let random=Math.random();
-    let numero=random*(max-min+1);
+    let numero = random * (max - min);
     let numeroEntero = Math.ceil(numero);
-    numeroEntero = numeroEntero+min-1;
-    return numeroEntero
+    numeroEntero = numeroEntero + min;
+    return numeroEntero;
+}
+
+function mostrarEnSpan(idSpan, valor){
+    let componente=document.getElementById(idSpan);
+        componente.textContent=valor;
 }
